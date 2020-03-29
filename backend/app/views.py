1c0c68e6ed2014964e7b9ef3aa5985dc9f60ce19
Ricart <ricarteusuo11@gmail.com>
2020-03-28 22:42:15 -0300

    fix(backend): correcting response from the ONG listing view and correcting imports

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,8 +1,9 @@
 import psycopg2
-
+import psycopg2.extras
 from flask import g
 
 from app import app
+
 
 # ~~~~
 @app.before_request
@@ -17,7 +18,10 @@ def teardown_request(exception):
 
 
 @app.route('/ongs', methods=['GET'])
-def ongs_list(request):
+def ongs_list():
     data = {}
-    data['ongs'] =
+    cur = g.db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute(f"SELECT * FROM ongs")
+    data['ongs'] = cur.fetchall()
+    cur.close()
     return data
