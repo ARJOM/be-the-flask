7a6928105e20570de0368b47bea736c83ffbf58c
Ricart <ricarteusuo11@gmail.com>
2020-03-29 00:20:01 -0300

    style: adding spacing between logic blocks

--- backend/app/views.py
+++ backend/app/views.py
@@ -6,6 +6,7 @@ from app import app
 
 from utils import generateUniqueId
 
+
 # ~~~~
 @app.before_request
 def before_request():
@@ -18,12 +19,15 @@ def teardown_request(exception):
 # ~~~~
 
 
+# Ong
+
 @app.route('/ongs', methods=['GET'])
 def ongs_list():
     cur = g.db.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cur.execute(f"SELECT * FROM ongs")
     ongs = cur.fetchall()
     cur.close()
+
     return jsonify(ongs)
 
 
@@ -31,13 +35,16 @@ def ongs_list():
 def ongs_new():
     if not request.json:
         abort(404)
+
     new = request.json
     id = generateUniqueId.generate_unique_id()
+
     cur = g.db.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cur.execute(f"INSERT INTO ongs(id, name, email, whatsapp, city, uf) "
                 f"VALUES ('{id}','{new['name']}','{new['email']}','{new['whatsapp']}','{new['city']}','{new['uf']}')")
     g.db.commit()
     cur.close()
+
     return jsonify({"id": id}), 201
 
 
@@ -49,6 +56,7 @@ def incidents_list():
     cur.execute(f"SELECT * FROM incident")
     incidents = cur.fetchall()
     cur.close()
+
     return jsonify(incidents)
 
 
@@ -56,11 +64,14 @@ def incidents_list():
 def incidents_new():
     if not request.json:
         abort(404)
+
     ong_id = request.headers['authorization']
     new = request.json
+
     cur = g.db.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cur.execute(f"INSERT INTO incidents(title, description, value, ong_id) "
                 f"VALUES ('{new['title']}','{new['description']}',{new['value']},'{ong_id}')")
     g.db.commit()
     cur.close()
+
     return {}, 201
